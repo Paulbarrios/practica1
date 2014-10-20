f8c2b6ed1ac5dad37576f485aa954bbea0b0d7e8
dam2 <pool.nada.mas@gmail.com>
2014-10-20 10:41:24 +0200

    commit 1

--- AdivinaNumero/src/com/example/adivinanumero/MainActivity.java
+++ AdivinaNumero/src/com/example/adivinanumero/MainActivity.java
@@ -4,12 +4,44 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+	private int intentos = 0;
+	private int numeroAdivinar = 0;
+	private static final String TAG = "Pulsame";
 
+
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// Obtenemos el objeto Button a partir de su id.
+		Boton = (Button) findViewById(R.id.button);
+		intentosText = (TextView) findViewById(R.id.textView1);
+		
+		// Nos hacemos observadores de sus pulsaciones.
+		Boton.setOnClickListener(
+			new View.OnClickListener() {
+				public void onClick(View v) {
+				verificarNumero();
+			}
+			}
+		);
+
 	}
+	
+	public void verificarNumero() {
+		 android.util.Log.e(TAG, "Pulsado");
+		 
+		 
+	}
+	
+	private Button Boton = null;
+	private TextView intentosText = null;
 }
