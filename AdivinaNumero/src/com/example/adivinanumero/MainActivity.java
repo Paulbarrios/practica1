0042565b42bfaaccf8a8792e4b00d52efa4d76a5
frou <frou@frou-pc>
2014-11-01 23:16:26 +0100

    Implementado volver a jugar con visibilidades

--- AdivinaNumero/src/com/example/adivinanumero/MainActivity.java
+++ AdivinaNumero/src/com/example/adivinanumero/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.adivinanumero;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -19,11 +21,17 @@ public class MainActivity extends Activity {
 
 		intentosText = (TextView) findViewById(R.id.textView1);
 		cajaTexto = (EditText) findViewById(R.id.editText1);
-		adivina = (TextView) findViewById(R.id.textView11);
+		adivina = (TextView) findViewById(R.id.textoPrincipal);
+		intentaloText = (TextView) findViewById(R.id.textView2);
+		textoVictoria  = (TextView) findViewById(R.id.textoVictoria);
+		volverAJugar = (Button) findViewById(R.id.jugarDeNuevo);
+		probar = (Button) findViewById(R.id.button);
+		
+		
 		restoreMe(savedInstanceState);
 		android.util.Log.e(TAG, numeroAdivinar+"");
 	}
-
+	
 	public void verificarNumero(View v) {
 		int numeroCaja = 0;
 		String numero = cajaTexto.getText().toString();
@@ -50,9 +58,7 @@ public class MainActivity extends Activity {
 				adivina.setText(menor);
 				cajaTexto.setText("");
 			} else if (numeroCaja == numeroAdivinar) {
-				String format = res.getString(R.string.igual);
-				String igual = String.format(format, numeroCaja);
-				adivina.setText(igual);
+				terminarJuego(res,numeroCaja);
 			}
 		} else {
 			adivina.setText(R.string.vacio);
@@ -68,28 +74,80 @@ public class MainActivity extends Activity {
         outState.putString("adivina", (String) adivina.getText());
         outState.putString("intentosText", (String) intentosText.getText());
         
+        outState.putInt("intentosText", intentosText.getVisibility());
+        outState.putInt("cajaTexto", cajaTexto.getVisibility());
+        outState.putInt("adivinaV", adivina.getVisibility());
+        outState.putInt("intentaloText", intentaloText.getVisibility());
+        outState.putInt("textoVictoria", textoVictoria.getVisibility());
+        outState.putInt("volverAJugar", volverAJugar.getVisibility());
+        outState.putInt("probar", probar.getVisibility());
+               
     }
   
-  private void restoreMe(Bundle state){
+	  private void restoreMe(Bundle state){
+	  
+	  	if (state!=null) {
+	  		intentos = state.getInt("intentos");
+	  		numeroAdivinar = state.getInt("numeroAdivinar");
+	  		adivina.setText(state.getString("adivina"));
+	  		intentosText.setText(state.getString("intentosText"));
+	  		
+	  		
+	  		android.content.res.Resources res = getResources();
+	  		String format = res.getString(R.string.igual);
+			String igual = String.format(format, state.getInt("numeroAdivinar"));
+			textoVictoria.setText(igual) ;		
+	  		
+	  		intentosText.setVisibility(state.getInt("intentosText"));
+			cajaTexto.setVisibility(state.getInt("cajaTexto"));
+			adivina.setVisibility(state.getInt("adivinaV"));
+			intentaloText.setVisibility(state.getInt("intentaloText"));
+			probar.setVisibility(state.getInt("probar"));
+			textoVictoria.setVisibility(state.getInt("textoVictoria"));
+			volverAJugar.setVisibility(state.getInt("volverAJugar"));
+	  	}else{
+	  		valoresACero();
+	  	}
+	   }
   
-  	if (state!=null) {
-  		intentos = state.getInt("intentos");
-  		numeroAdivinar = state.getInt("numeroAdivinar");
-  		adivina.setText(state.getString("adivina"));
-  		intentosText.setText(state.getString("intentosText"));
-  	}else{
-  		valoresACero();
-  	}
-   }
-  
-  public void valoresACero() {
-	  intentos = 0;
-	  numeroAdivinar = (int) Math.round(Math.random() * 100);
-  }
+	  public void valoresACero() {
+		  intentos = 0;
+		  numeroAdivinar = (int) Math.round(Math.random() * 100);
+	  }
+	  
+	  public void terminarJuego(Resources res, int numeroCaja) {
+		  String format = res.getString(R.string.igual);
+		  String igual = String.format(format, numeroCaja);
+		  intentosText.setVisibility(View.INVISIBLE);
+		  cajaTexto.setVisibility(View.INVISIBLE);
+		  adivina.setVisibility(View.INVISIBLE);
+		  intentaloText.setVisibility(View.INVISIBLE);
+		  probar.setVisibility(View.INVISIBLE);
+		  textoVictoria.setText(igual);
+		  textoVictoria.setVisibility(View.VISIBLE);
+		  volverAJugar.setVisibility(View.VISIBLE);
+		  
+	  }
+	  
+	  public void reiniciarJuego(View v) {
+		 valoresACero();
+		 cajaTexto.setText("");
+		 adivina.setText(R.string.adivina);
+		  cajaTexto.setVisibility(View.VISIBLE);
+		  adivina.setVisibility(View.VISIBLE);
+		  intentaloText.setVisibility(View.VISIBLE);
+		  probar.setVisibility(View.VISIBLE);
+		  textoVictoria.setVisibility(View.INVISIBLE);
+		  volverAJugar.setVisibility(View.INVISIBLE);
+	}
 
 
 
 	private TextView adivina;
 	private TextView intentosText;
 	private EditText cajaTexto;
+	private TextView intentaloText;
+	private TextView textoVictoria;
+	private Button volverAJugar;
+	private Button probar;
 }
